93ff3dad90b6d1415e881d741eb0bad208e1586f
ZeynelTok <48226143+ZeynelTok@users.noreply.github.com>
2021-04-11 18:30:21 +0000

    Update addBlog.php

--- webroot/project/addBlog.php
+++ webroot/project/addBlog.php
@@ -4,7 +4,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 {
     include 'db.php';   
     $sql_query = "INSERT INTO blogs (id, dateandtime, title, maintext) VALUES ('1', CURRENT_TIMESTAMP(), ".$_POST['title'].", ".$_POST['maintext'].")";
+    echo $sql_query;
     if(mysqli_query($conn, $sql_query)){
+        echo ('123');
         header('Location: blog.php');
         exit;
     }
