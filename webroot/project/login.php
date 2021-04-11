f898604c8474efaf53399755e8cc574429463593
ZeynelTok <48226143+ZeynelTok@users.noreply.github.com>
2021-04-11 00:35:55 +0000

    Update login.php

--- webroot/project/login.php
+++ webroot/project/login.php
@@ -4,11 +4,13 @@ if(isset($_POST['save']))
 {
     extract($_POST);
     include 'db.php';
-    $sql=mysqli_query($conn,"SELECT * FROM users where Email='$email' and Password='$password');
+    $sql=mysqli_query($conn,"SELECT * FROM users where email='$email' and password='$password');
     $row  = mysqli_fetch_array($sql);
+    console.log($email);
+    console.log($password);
     if(is_array($row))
     {
-        $_SESSION["ID"] = $row['ID'];
+        $_SESSION["ID"]=$row['ID'];
         $_SESSION["firstName"]=$row['firstName'];
         $_SESSION["lastName"]=$row['lastName'];
         $_SESSION["email"]=$row['email']; 
