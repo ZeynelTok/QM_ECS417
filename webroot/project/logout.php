e0e256a8c7f8107bd7d760a71102665c526f829c
ZeynelTok <48226143+ZeynelTok@users.noreply.github.com>
2021-04-11 17:18:55 +0000

    Update logout.php

--- webroot/project/logout.php
+++ webroot/project/logout.php
@@ -1,7 +1,12 @@
 <?php
 session_start();
 session_unset();
+unset($_SESSION["ID"]);
+unset($_SESSION["firstName"]);
+unset($_SESSION["lastName"]);
+unset($_SESSION["email"]); 
 session_destroy();
+$disable = true;
 header('Location: blog.php');
 exit;
 ?>
