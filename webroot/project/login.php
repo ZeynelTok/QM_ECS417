0b43a3e12d596adf4b61c872dfd12da0fb7e8b26
ZeynelTok <48226143+ZeynelTok@users.noreply.github.com>
2021-04-11 00:48:46 +0000

    Update login.php

--- webroot/project/login.php
+++ webroot/project/login.php
@@ -4,7 +4,7 @@ if(isset($_POST['save']))
 {
     extract($_POST);
     include 'db.php';
-    $sql=mysqli_query($conn,"SELECT * FROM users where email='$email' and password='$password');
+    $sql=mysqli_query($conn,"SELECT * FROM users where email='$email' and password='$password'");
     $row  = mysqli_fetch_array($sql);
     console.log($email);
     console.log($password);
