2054c97e068c13e5f13943d53a09c13eb7983d49
ZeynelTok <48226143+ZeynelTok@users.noreply.github.com>
2021-04-11 02:22:02 +0000

    Update login.php

--- webroot/project/login.php
+++ webroot/project/login.php
@@ -2,23 +2,25 @@
 session_start();
 if(isset($_POST['save']))
 {
+    echo '123';
     extract($_POST);
     include 'db.php';
     $sql=mysqli_query($conn,"SELECT * FROM users where email='$email' and password='$password'");
     $row  = mysqli_fetch_array($sql);
     if(is_array($row))
     {
+        echo '456';
         $_SESSION["ID"]=$row['ID'];
         $_SESSION["firstName"]=$row['firstName'];
         $_SESSION["lastName"]=$row['lastName'];
         $_SESSION["email"]=$row['email']; 
-        header('Location: ' . $_SERVER['HTTP_REFERER'];);
+        header('Location: blog.php');
         exit;
     }
     else
     {
         echo "Invalid Email/Password";
-        header('Location: ' . $_SERVER['HTTP_REFERER'];);
+        header('Location: blog.php');
         exit;
     }
 
