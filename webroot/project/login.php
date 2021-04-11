1ed61669e765f8f0882a98b184bb494a5a0f0df7
ZeynelTok <48226143+ZeynelTok@users.noreply.github.com>
2021-04-11 14:36:30 +0000

    Update login.php

--- webroot/project/login.php
+++ webroot/project/login.php
@@ -7,26 +7,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     $sql_query = "select count(*) as count from users where email='".$email."' and password='".$password."'";
         $result = mysqli_query($conn,$sql_query);
         $row = mysqli_fetch_array($result);
-        echo $row;
+        echo $sql_query;
         $count = $row['count'];
         echo $count;
         if($count > 0){
-        echo '456';
-        $_SESSION["ID"]=$row['ID'];
-        $_SESSION["firstName"]=$row['firstName'];
-        $_SESSION["lastName"]=$row['lastName'];
-        $_SESSION["email"]=$row['email']; 
-        //header('Location: blog.php');
-        //exit;
-    }
-    else
-    {
-        echo "Invalid Email/Password";
-        //header('Location: blog.php');
-        //exit;
-    }
-    $conn->close();
-
-    
+            echo '456';
+            $_SESSION["ID"]=$row['ID'];
+            $_SESSION["firstName"]=$row['firstName'];
+            $_SESSION["lastName"]=$row['lastName'];
+            $_SESSION["email"]=$row['email']; 
+            //header('Location: blog.php');
+            //exit;
+        }
+        else
+        {
+            echo "Invalid Email/Password";
+            //header('Location: blog.php');
+            //exit;
+        }
+        $conn->close();   
 }
 ?>
